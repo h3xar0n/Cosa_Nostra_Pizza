905e2ccbb1af1d0d43403fcddb6e5ba5690dbf8a
Aron Eidelman <aron@eideldev.com>
2018-04-14 14:44:00 -0700

    Basic footer

---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <footer class="row">
+    <nav class="navbar navbar-toggleable-md navbar-light">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="" class="nav-link">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">MENU</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">ADMIN</a>
+        </li>
+      </ul>
+    </nav>
+  </footer>
+</template>
