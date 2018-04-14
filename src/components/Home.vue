c7ea046a7f67e90de2d5a486beaf2d7b6172f273
Aron Eidelman <aron@eideldev.com>
2018-04-14 14:59:02 -0700

    Add home component

---
+++ src/components/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="row">
+    <div id="home" class="col-sm-12 text-center">
+      <div class="background">
+        <h1>Welcome to Cosa Nostra Pizza</h1>
+        <h2>You've Got a Friend in the Family</h2>
+        <button class="btn btn-success">Hit it.</button>
+      </div>
+    </div>
+  </div>
+</template>
