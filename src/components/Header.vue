fbc5c8d4e17a63fdf868bde641747dead6e4932c
Aron Eidelman <aron@eideldev.com>
2018-04-14 14:42:45 -0700

    Basic navbar

---
+++ src/components/Header.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="row">
+    <nav class="navbar navbar-toggleable-md navbar-light">
+      <a href="/" class="navbar-brand">COSA NOSTRA PIZZA</a>
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a href="" class="nav-link">HOME</a>
+        </li>
+        <li class="nav-item">
+          <a href="" class="nav-link">MENU</a>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
