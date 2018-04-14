9428e9cc921d9330d5f4c4cffb0414d529b5deaa
Aron Eidelman <aron@eideldev.com>
2018-04-14 15:02:32 -0700

    Import and render main components in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,28 @@
 <template>
   <div id="app">
-
+    <div class="container">
+      <cnp-header></cnp-header>
+    </div>
+    <div class="container">
+      <cnp-home></cnp-home>
+    </div>
+    <div class="container">
+      <cnp-footer></cnp-footer>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Home from './components/Home.vue'
 
+export default {
+  components: {
+    cnpHeader: Header,
+    cnpFooter: Footer,
+    cnpHome: Home
+  }
 }
 </script>
 
