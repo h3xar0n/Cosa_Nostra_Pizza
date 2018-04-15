1e80e11c143f4dfce69754ece0580cdadde7ddcb
Aron Eidelman <aron@eideldev.com>
2018-04-15 10:25:14 -0700

    Install and config Firebase

---
+++ src/firebaseConfig.js
@@ -0,0 +1,13 @@
+import Firebase from 'firebase'
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyD_lNkhHUlCD90lB7piWhFD0ZUe4zxDEtI",
+  authDomain: "cosa-nostra-pizza.firebaseapp.com",
+  databaseURL: "https://cosa-nostra-pizza.firebaseio.com",
+  projectId: "cosa-nostra-pizza",
+  storageBucket: "",
+  messagingSenderId: "712760686947"
+};
+const firebaseApp =      Firebase.initializeApp(config);
+const db =               firebaseApp.database();
+export const dbMenuRef = db.ref('menu');
