429bd57c0252f16dd3c821c8f0dcd533b3ae2856
Aron Eidelman <aron@eideldev.com>
2018-04-15 10:45:52 -0700

    Enable Firebase authentication

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -25,38 +25,46 @@
 
     <div class="row">
       <h3>Current orders:</h3>
-        <table class="table table-sm">
-          <thead class="thead-default">
-            <tr>
-              <th>Item</th>
-              <th>Size</th>
-              <th>Quantity</th>
-              <th>Price</th>
-            </tr>
-          </thead>
-          <tbody>
-            <div class="order-number">
-              <strong><em>Order Number: 1</em></strong>
-              <button class="btn btn-outline-danger btn-sm">X</button>
-            </div>
-            <tr>
-              <td>Margherita</td>
-              <td>9"</td>
-              <td>1</td>
-              <td>$6.95</td>
-            </tr>
-          </tbody>
-        </table>
+      <table class="table table-sm">
+        <thead class="thead-default">
+          <tr>
+            <th>Item</th>
+            <th>Size</th>
+            <th>Quantity</th>
+            <th>Price</th>
+          </tr>
+        </thead>
+        <tbody>
+          <div class="order-number">
+            <strong><em>Order Number: 1</em></strong>
+            <button class="btn btn-outline-danger btn-sm">X</button>
+          </div>
+          <tr>
+            <td>Margherita</td>
+            <td>9"</td>
+            <td>1</td>
+            <td>$6.95</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+    <hr>
+    <div class="row">
+      <div class="col-sm-12 col-lg-6">
+        <cnp-login></cnp-login>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import NewPizza from './NewPizza.vue'
+import Login from './Login.vue'
 
 export default {
   components: {
-    cnpNewPizza: NewPizza
+    cnpNewPizza: NewPizza,
+    cnpLogin: Login
   }  
 }
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <form>
+      <div class="form-group">
+        <label for="name">Email Address</label>
+        <input type="email" class="form-control" id="email" placeholder="Enter email">
+      </div>
+      <div class="form-group">
+        <label for="name">Password</label>
+        <input type="password" class="form-control" id="password" placeholder="Enter password">
+      </div>
+      <div class="row">
+        <button type="button" 
+                class="btn btn-primary" 
+                @click.prevent="signIn">Sign in</button>
+        <button type="button" 
+                class="btn btn-danger"
+                @click.prevent="signOut">Sign out</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+  import Firebase from 'firebase'
+
+</script>
