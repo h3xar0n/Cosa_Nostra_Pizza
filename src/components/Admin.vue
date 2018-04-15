575313c53e8d758e9bf6aad647478c6bde23982e
Aron Eidelman <aron@eideldev.com>
2018-04-15 09:33:43 -0700

    Add order tracking section

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,27 +1,52 @@
 <template>
-  <div class="row">
-    <div class="col-sm-12 col-md-6">
-      <!-- New Pizza Component -->
+  <div>
+    <div class="row">
+      <div class="col-sm-12 col-md-6">
+        <!-- New Pizza Component -->
+      </div>
+      <div class="col-sm-12 col-md-6">
+        <h3>Menu:</h3>
+        <table class="table table-hover">
+          <thead class="thead-default">
+            <tr>
+              <th>Item</th>
+              <th>Remove from Menu</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Margherita</td>
+              <td><button class="btn btn-outline-danger btn-sm">X</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <div class="col-sm-12 col-md-6">
-      <h3>Menu:</h3>
-      <table class="table table-hover">
-        <thead class="thead-default">
-          <tr>
-            <th>Item</th>
-            <th>Remove from Menu</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>Margherita</td>
-            <td><button class="btn btn-outline-danger btn-sm">X</button></td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="col-sm-12 col-md-6">
-      
+
+    <div class="row">
+      <h3>Current orders:</h3>
+        <table class="table table-sm">
+          <thead class="thead-default">
+            <tr>
+              <th>Item</th>
+              <th>Size</th>
+              <th>Quantity</th>
+              <th>Price</th>
+            </tr>
+          </thead>
+          <tbody>
+            <div class="order-number">
+              <strong><em>Order Number: 1</em></strong>
+              <button class="btn btn-outline-danger btn-sm">X</button>
+            </div>
+            <tr>
+              <td>Margherita</td>
+              <td>9"</td>
+              <td>1</td>
+              <td>$6.95</td>
+            </tr>
+          </tbody>
+        </table>
     </div>
   </div>
 </template>
