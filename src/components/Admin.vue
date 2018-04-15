a9322a50b79135bfe7e4496bbcbef4179dfb8d3c
Aron Eidelman <aron@eideldev.com>
2018-04-14 22:42:19 -0700

    New Admin table

---
+++ src/components/Admin.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12 col-md-6">
+      <!-- New Pizza Component -->
+    </div>
+    <div class="col-sm-12 col-md-6">
+      <h3>Menu:</h3>
+      <table class="table table-hover">
+        <thead class="thead-default">
+          <tr>
+            <th>Item</th>
+            <th>Remove from Menu</th>
+          </tr>
+        </thead>
+      </table>
+    </div>
+    <div class="col-sm-12 col-md-6">
+      
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
