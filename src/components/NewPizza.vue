5e1b78d80b5d31664c607e4578c28d4f62288e72
Aron Eidelman <aron@eideldev.com>
2018-04-15 10:06:05 -0700

    Form input bindings

--- src/components/NewPizza.vue
+++ src/components/NewPizza.vue
@@ -5,49 +5,85 @@
     <div class="form-group row">
       <label for="name" class="col-sm-3">Name</label>
       <div class="col-sm-9">
-        <input type="text" class="form-control" value="">
+        <input type="text" class="form-control" v-model="newPizza.name">
       </div>
     </div>
 
     <div class="form-group row">
       <label for="description" class="col-sm-3">Description</label>
       <div class="col-sm-9">
-        <textarea type="text" class="form-control" value="" rows="5"></textarea>
+        <textarea type="text" 
+                  class="form-control" 
+                  value="" 
+                  rows="5"
+                  v-model="newPizza.description"></textarea>
       </div>
     </div>
 
-    <p><strong>Option 1:</strong></p>
+    <p>
+      <strong>Option 1:</strong>
+    </p>
     <div class="form-group row">
       <label for="size" class="col-sm-3">Size (")</label>
       <div class="col-sm-9">
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="newPizza.options[0].size">
       </div>
     </div>
 
     <div class="form-group row">
       <label for="price" class="col-sm-3">Price</label>
       <div class="col-sm-9">
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="newPizza.options[0].price">
       </div>
     </div>
 
-    <p><strong>Option 2:</strong></p>
+    <p>
+      <strong>Option 2:</strong>
+    </p>
     <div class="form-group row">
       <label for="size" class="col-sm-3">Size (")</label>
       <div class="col-sm-9">
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="newPizza.options[1].size">
       </div>
     </div>
 
     <div class="form-group row">
       <label for="price" class="col-sm-3">Price</label>
       <div class="col-sm-9">
-        <input type="text" class="form-control">
+        <input type="text" class="form-control" v-model="newPizza.options[1].price">
       </div>
     </div>
 
     <div class="formt-group row">
-      <button type="button" class="btn btn-success btn-block">Add</button>
+      <button type="button" 
+              class="btn btn-success btn-block"
+              @click="addMenuItem">Add</button>
     </div>
   </form>
 </template>
+
+<script>
+  export default {
+    data() {
+      return {
+        newPizza: {
+          'name': 'Omerta',
+          'description': 'Eg. A delicious tomato based pizza topped with mozzarella',
+          'options': [{
+            'size': 9,
+            'price': 6.95
+          }, {
+            'size': 12,
+            'price': 10.95
+          }]
+        }
+      }
+    },
+    methods: {
+      addMenuItem() {
+        console.log(this.newPizza)
+      }
+    }
+  }
+
+</script>
