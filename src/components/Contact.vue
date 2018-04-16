73d499134d4c46f45d77eaa4968cac2a80583664
h3xar0n <you@example.com>
2018-04-16 16:43:33 -0700

    Filler for contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="card" style="width: 18rem;">
-    <div class="card-body">
-      <h5 class="card-title">Contact</h5>
-      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  <div class="card">
+    <div class="card-block">
+      <h4 class="card-title">Contact</h4>
+      <p class="card-text">
+        <address>
+          <strong>Pizza Planet</strong><br>
+          Address line 1<br>
+          Address line 2<br>
+          Phone: (123) 456-7890
+        </address>
+        <address>
+          <strong>Email:</strong><br>
+          <p>contact@pizzaplanet.com</p>
+        </address>
+      </p>
     </div>
   </div>
 </template>
