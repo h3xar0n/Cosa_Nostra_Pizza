a2e36265b00e99cd1a423bfdc2c1bdf86d894dc1
h3xar0n <you@example.com>
2018-04-16 16:44:10 -0700

    Some character to the contact

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -4,14 +4,14 @@
       <h4 class="card-title">Contact</h4>
       <p class="card-text">
         <address>
-          <strong>Pizza Planet</strong><br>
+          <strong>Cosa Nostra Pizza</strong><br>
           Address line 1<br>
           Address line 2<br>
           Phone: (123) 456-7890
         </address>
         <address>
           <strong>Email:</strong><br>
-          <p>contact@pizzaplanet.com</p>
+          <p>aron@eideldev.com</p>
         </address>
       </p>
     </div>
