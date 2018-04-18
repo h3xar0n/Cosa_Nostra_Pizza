492298e518428c0d84a2db4cd989bcf0e4488d22
Aron Eidelman <aron@eideldev.com>
2018-04-17 17:17:46 -0700

    Router view for child links

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,3 +1,22 @@
 <template>
-  <h1>About Us</h1>
+  <div>
+    <h2>"You've got a friend in The Family"</h2>
+    <nav class="navbar navbar-toggleable-md navbar-light">
+      <ul class="navbar-nav">
+        <router-link to="/history" tag="li">
+          <a class="nav-link">HISTORY</a>
+        </router-link>
+        <router-link to="/contact" tag="li">
+          <a class="nav-link">CONTACT</a>
+        </router-link>
+        <router-link to="/ordering-guide" tag="li">
+          <a class="nav-link">ORDER </a>
+        </router-link>
+        <router-link to="/delivery" tag="li">
+          <a class="nav-link">HIGH-SPEED DELIVERY</a>
+        </router-link>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
 </template>
