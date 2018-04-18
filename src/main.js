71b53a69ddc8bc93ac817d0d6befb421ddec8e9e
Aron Eidelman <aron@eideldev.com>
2018-04-18 12:54:33 -0700

    Refactor routes

--- src/main.js
+++ src/main.js
@@ -1,36 +1,20 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from './components/Home.vue'
-import Menu from './components/Menu.vue'
-import About from './components/About.vue'
-import Contact from './components/Contact.vue'
-import History from './components/History.vue'
-import Delivery from './components/Delivery.vue'
-import OrderingGuide from './components/OrderingGuide.vue'
-import Admin from './components/Admin.vue'
+import { routes } from './routes'
 import App from './App.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  { path: '/',      name: 'homeLink',  component: Home },
-  { path: '/menu',  name: 'menuLink',  component: Menu },
-  { path: '/admin', name: 'adminLink', component: Admin, beforeEnter: (to, from, next) => {
-    alert('This area is for friends of ours only, please introduce yourself to continue.')
-    next()
-  }},
-  { path: '/about', name: 'aboutLink', component: About, children: [
-    { path: '/contact',  name: 'contactLink',       component: Contact },
-    { path: '/history',  name: 'historyLink',       component: History },
-    { path: '/delivery', name: 'deliveryLink',      component: Delivery },
-    { path: '/ordering', name: 'orderingGuideLink', component: OrderingGuide }
-  ] },
-  { path: '*', redirect: '/' }
-]
-
 const router = new VueRouter({
   routes,
-  mode: 'history'
+  mode: 'history',
+  scrollBehavior(to, from, savedPosition) {
+    if(to.hash) {
+     selector: to.hash
+    } else {
+      return { x: 0, y: 0 }
+    }
+  }
 })
 
 new Vue({
