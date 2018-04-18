90a2db36b2fd742c1f1fe3792d2622203f084d8e
Aron Eidelman <aron@eideldev.com>
2018-04-18 13:00:23 -0700

    Named router views

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,19 @@
     </div>
     <router-view></router-view>
     <div class="container">
+      <div class="row">
+        <div class="col-sm-12 col-md-4">
+          <router-view name="ordering-guide"></router-view>
+        </div>      
+        <div class="col-sm-12 col-md-4">
+          <router-view name="delivery"></router-view>
+        </div>     
+        <div class="col-sm-12 col-md-4">
+          <router-view name="history"></router-view>
+        </div>       
+      </div>
+    </div>
+    <div class="container">
       <cnp-footer></cnp-footer>
     </div>
   </div>
