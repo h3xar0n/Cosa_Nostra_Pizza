d6a65b9ea4e11976ab9b1350fc273ff2d5e77aef
Aron Eidelman <aron@eideldev.com>
2018-04-19 20:00:05 -0700

    Remove items from menu and orders in database

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -16,7 +16,12 @@
           <tbody :key="item['.key']" v-for="item in getMenuItems">
             <tr>
               <td>{{ item.name }}</td>
-              <td><button class="btn btn-outline-danger btn-sm">X</button></td>
+              <td>
+                  <button class="btn btn-outline-danger btn-sm"
+                          @click="removeMenuItem(item['.key'])">
+                    X
+                  </button>
+              </td>
             </tr>
           </tbody>
         </table>
@@ -39,7 +44,8 @@
         <tbody>
           <div class="order-number">
             <strong><em>Order Number: {{ index + 1 }}</em></strong>
-            <button class="btn btn-outline-danger btn-sm">X</button>
+            <button class="btn btn-outline-danger btn-sm"
+                    @click="removeOrderItem(orders['.key'])">X</button>
           </div>
           <tr :key="orderItems" v-for="orderItems in orders['.value']">
             <td>{{ orderItems.name }}</td>
@@ -63,6 +69,7 @@
 import NewPizza from './NewPizza.vue'
 import Login from './Login.vue'
 import { mapGetters } from 'vuex'
+import { dbMenuRef, dbOrdersRef } from '../firebaseConfig'
 
 export default {
   components: {
@@ -75,6 +82,14 @@ export default {
       'getMenuItems',
       'getOrders'
     ])
+  },
+  methods: {
+    removeMenuItem(key) {
+      dbMenuRef.child(key).remove()
+    },
+    removeOrderItem(key) {
+      dbOrdersRef.child(key).remove()
+    }
   },
   beforeRouteLeave: (to, from, next) => {
     if(confirm("The moment you try to get out, we'll pull you back in!") === true ) {
