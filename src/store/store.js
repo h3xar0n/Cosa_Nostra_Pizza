fa14731ed0e8bf9ccb523dbeeb7a5e5995b75604
Aron Eidelman <aron@eideldev.com>
2018-04-19 19:08:09 -0700

    Import modules to state

--- src/store/store.js
+++ src/store/store.js
@@ -1,53 +1,16 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-import * as actions from './actions'
-import * as getters from './getters'
-import * as mutations from './mutations'
+import menu from './modules/menu'
+import orders from './modules/orders'
+import users from './modules/users'
 
 Vue.use(Vuex)
 
 export const store = new Vuex.Store({
-  state: {
-    menuItems: {
-      1: {
-        'name': 'Omerta',
-        'description': 'A delicious tomato based pizza topped with mozzarella',
-        'options': [{
-          'size': 9,
-          'price': 6.95
-        }, {
-          'size': 12,
-          'price': 10.95
-        }]
-      },
-      2: {
-        'name': 'Poor Impulse Control',
-        'description': 'A delicious tomato based pizza topped with mozzarella and pepperoni',
-        'options': [{
-          'size': 9,
-          'price': 7.95
-        }, {
-          'size': 12,
-          'price': 12.95
-        }]
-      },
-      3: {
-        'name': 'Pearly Gates',
-        'description': 'A delicious tomato based pizza topped with mozzarella, ham and pineapple',
-        'options': [{
-          'size': 9,
-          'price': 7.95
-        }, {
-          'size': 12,
-          'price': 12.95
-        }]
-      }
-    },
-    orders: [],
-    currentUser: null
-  },
-  getters,
-  mutations,
-  actions
+  modules: {
+    menu,
+    orders,
+    users
+  }
 })
